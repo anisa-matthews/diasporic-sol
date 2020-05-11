bbf504b8feb3db87fe6219352b11922332342027
Anisa <neep@Anisas-MacBook-Air.local>
2020-05-11 13:24:26 -0700

    oop

--- server/src/app.js
+++ server/src/app.js
@@ -150,4 +150,7 @@ app.delete('/entries/:id', (req, res) => {
 // 	)
 // })
 
-app.listen(process.env.PORT || 8081)
+app.listen(process.env.PORT || 8081, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
+});
